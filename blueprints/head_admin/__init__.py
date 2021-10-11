e585faf02102b83174af38dff503b515a8a1b2b1
Pravoslav <pravoslav.zilka@gmail.com>
2021-10-11 14:58:57 +0200

    operations for admin page added

--- blueprints/head_admin/__init__.py
+++ blueprints/head_admin/__init__.py
@@ -1,3 +1,61 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from models import TicketTypeType, TicketType
+import datetime
+from database import db_session
 
 h_admin_bp = Blueprint("h_admin_bp", __name__, template_folder="templates", static_folder="static")
+
+
+@h_admin_bp.route("/operations/")
+def operations():
+    ttts = TicketTypeType.query.all()
+    return render_template("head_admin/head_operations.html", ttts=ttts)
+
+
+@h_admin_bp.route("/add-piece/", methods=['POST'])
+def add_piece():
+    name = request.form["piece-name"]
+    speaker = request.form["piece-speaker"]
+    start = datetime.datetime.strptime(request.form["piece-start"], '%H:%M').time()
+    max_cap = int(request.form["piece-cap"])
+    ttt = TicketTypeType.query.filter(TicketTypeType.id == int(request.form["piece-ttt"])).first()
+
+    tt = TicketType(name, speaker, max_cap)
+    tt.start = start
+    tt.ticket_type_type = ttt
+
+    db_session.add(tt)
+    db_session.commit()
+
+    flash(f"Nová časť programu v {ttt.name} bola pridaná", "success")
+    return redirect(url_for("h_admin_bp.operations"))
+
+
+@h_admin_bp.route("/edit-piece/", methods=['POST'])
+def edit_piece():
+    name = request.form["piece-name"]
+    speaker = request.form["piece-speaker"]
+    start = datetime.datetime.strptime(request.form["piece-start"], '%H:%M:%S').time()
+    max_cap = int(request.form["piece-cap"])
+    tt = TicketType.query.filter(TicketType.id == int(request.form["piece-id"])).first()
+
+    tt.name = name
+    tt.speaker = speaker
+    tt.start = start
+    tt.max_cap = max_cap
+
+    db_session.commit()
+
+    flash(f"Časť programu '{tt.name}' bola upravená", "success")
+    return redirect(url_for("h_admin_bp.operations"))
+
+
+@h_admin_bp.route("/delete-piece/<int:piece_id>/")
+def delete_piece(piece_id):
+    tt = TicketType.query.filter(TicketType.id == piece_id).first()
+
+    db_session.delete(tt)
+    db_session.commit()
+
+    flash(f"Časť programu '{tt.name}' bola vymazaná", "success")
+    return redirect(url_for("h_admin_bp.operations"))
