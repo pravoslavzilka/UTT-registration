e96883da0ffc90178537cc9a86db6d2093a83c04
pravoslavzilka <62677181+pravoslavzilka@users.noreply.github.com>
2021-10-15 13:21:23 +0200

    Update __init__.py

--- blueprints/admin/__init__.py
+++ blueprints/admin/__init__.py
@@ -1,4 +1,22 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
+from models import TicketTypeType, TicketType, Ticket
 
 admin_bp = Blueprint("admin_bp", __name__, template_folder="templates", static_folder="static")
 
+
+@admin_bp.route("/check-tickets/")
+def check_tickets_view():
+    ttts = TicketTypeType.query.all()
+    return render_template("admin/tickets_check_menu.html", ttts=ttts)
+
+
+@admin_bp.route("/scan-tickets/<int:tt_id>/")
+def tickets_scan(tt_id):
+    tt = TicketType.query.filter(TicketType.id == tt_id).first()
+
+    if tt:
+        tickets = Ticket.query.filter(Ticket.ticket_type == tt).all()
+        list_of_users = [ticket.user.code for ticket in tickets]
+        return render_template("admin/tickets_scan.html", list_of_users=list_of_users, tt=tt)
+
+    return redirect(url_for("admin.check_tickets_view"))
