c31c3de37766ae7a86306e771157b29aa3fd5d78
Pravoslav <pravoslav.zilka@gmail.com>
2021-10-19 23:22:02 +0200

    sign up page for admin + finctions

--- blueprints/admin/__init__.py
+++ blueprints/admin/__init__.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template, redirect, url_for, session, flash
+from flask import Blueprint, render_template, redirect, url_for, session, flash, request
 from models import TicketTypeType, TicketType, Ticket, User
 from functools import wraps
 from database import db_session
+import uuid
 
 admin_bp = Blueprint("admin_bp", __name__, template_folder="templates", static_folder="static")
 
@@ -48,3 +49,95 @@ def tickets_scan(tt_id):
         return render_template("admin/tickets_scan.html", list_of_users=list_of_users, tt=tt)
 
     return redirect(url_for("admin.check_tickets_view"))
+
+
+@admin_bp.route("/user/<int:user_id>/")
+@check_admin
+def admin_user_page(user_id):
+    user = User.query.filter(User.id == user_id).first()
+    ttts = TicketTypeType.query.all()
+    tts = TicketType.query.all()
+    return render_template("admin/admin_user_page.html", user=user, tts=tts, ttts=ttts)
+
+
+@admin_bp.route("/delete-user/<int:user_id>/")
+@check_admin
+def delete_user(user_id):
+    u = User.query.filter(User.id == user_id).first()
+    db_session.delete(u)
+    db_session.commit()
+
+    flash(f"User {u.name} bol odstránený", "success")
+    return redirect(url_for("admin_bp.admin_sign_up"))
+
+
+@admin_bp.route("/public-sign-up/", methods=['GET'])
+@check_admin
+def admin_sign_up():
+    tts = TicketType.query.all()
+    return render_template("admin/admin_sign_up.html", tts=tts)
+
+
+@admin_bp.route("/public-sign-up/", methods=['POST'])
+@check_admin
+def sign_up_fun():
+    name = request.form["user-name"]
+    age = request.form["user-age"]
+    email = request.form["user-email"]
+    password = request.form["user-password"]
+    city = request.form["user-city"]
+    otp = request.form["user-otp"]
+    who = request.form["user-who"]
+    where = request.form["user-where"]
+
+    block1 = TicketType.query.filter(TicketType.name == request.form["user-block1"]).first()
+    block2 = TicketType.query.filter(TicketType.name == request.form["user-block2"]).first()
+    block3 = TicketType.query.filter(TicketType.name == request.form["user-block3"]).first()
+
+    user = User.query.filter(User.email == email).first()
+
+    if user:
+        flash("Tento email je už používaný iným uživateľom", "danger")
+        return redirect(url_for("admin_bp.admin_sign_up"))
+
+    user = User(name, email)
+    user.city = city
+    user.age = age
+    user.otp = otp
+    user.who = who
+    user.where = where
+    user.code = uuid.uuid4().hex
+    user.confirm = True
+
+    if request.form.get("user-news"):
+        user.news = True
+    else:
+        user.news = False
+
+    if block1:
+        if block1.max_cap > len(Ticket.query.filter(Ticket.ticket_type == block1).all()):
+            ticket1 = Ticket(block1, user)
+            db_session.add(ticket1)
+    if block2:
+        if block2.max_cap > len(Ticket.query.filter(Ticket.ticket_type == block2).all()):
+            ticket2 = Ticket(block2, user)
+            db_session.add(ticket2)
+    if block3:
+        if block3.max_cap > len(Ticket.query.filter(Ticket.ticket_type == block2).all()):
+            ticket3 = Ticket(block3, user)
+            db_session.add(ticket3)
+
+    db_session.add(user)
+    db_session.commit()
+
+    ttt = TicketTypeType.query.filter(TicketTypeType.name == "Ďaľší Program").first()
+    tts = TicketType.query.filter(TicketType.ticket_type_type == ttt).all()
+
+    for tt in tts:
+        if request.form.get("user-"+str(tt.id)):
+            ticket = Ticket(tt, user)
+            db_session.add(ticket)
+            db_session.commit()
+
+    flash("User bol vytvorený", "success")
+    return redirect(url_for("admin_bp.admin_user_page", user_id=user.id))
