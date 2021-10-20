bc1efedfece3a406f7fe1344793d57bb1882d2d7
Pravoslav <pravoslav.zilka@gmail.com>
2021-10-21 01:15:49 +0200

    admin change user profile

--- database.py
+++ database.py
@@ -3,8 +3,8 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-engine = create_engine('mysql+pymysql://doadmin:EatCxJLa62yQV5wh@db-mysql-fra1-44732-do-user-9796066-0.b.db.ondigitalocean.com:25060/defaultdb', connect_args={'ssl': {'ssl-mode': 'required'}}, pool_recycle=3600)
-#engine = create_engine('sqlite:///database.db', convert_unicode=True)
+# engine = create_engine('mysql+pymysql://doadmin:EatCxJLa62yQV5wh@db-mysql-fra1-44732-do-user-9796066-0.b.db.ondigitalocean.com:25060/defaultdb', connect_args={'ssl': {'ssl-mode': 'required'}}, pool_recycle=3600)
+engine = create_engine('sqlite:///database.db', convert_unicode=True)
 
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
